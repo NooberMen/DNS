645a14ccc0b1c75ea630799c5d4624961a43aa21
SpAGeTTi <0spagetti0@mail.ru>
2020-11-24 19:55:28 +0300

    Добавьте файлы проекта.

---
+++ DNS/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using DNS.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DNS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public  ActionResult Index()
+        {
+            using (DNSContext db = new DNSContext())
+            {
+                Product product1 = new Product { Name = "Asus" };
+
+                db.Products.Add(product1);
+                db.SaveChanges();
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public string PostData(string SName)
+        {
+            return "Параметр запроса" + SName;
+        }
+ 
+    }
+}
---
+++ DNS/Models/DNSContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace DNS.Models
+{
+    public class DNSContext : DbContext
+    {
+        public DNSContext() : base("DbConnection")
+        {}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
